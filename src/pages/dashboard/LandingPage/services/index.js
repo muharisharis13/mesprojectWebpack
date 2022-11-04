73f5380bb7a22892b1fb43242b75d59614ad4fe8
muharisharis13 <muharisharis13@gmail.com>
2022-11-05 02:26:50 +0700

    change lalala

--- src/pages/dashboard/LandingPage/services/index.js
+++ src/pages/dashboard/LandingPage/services/index.js
@@ -8,7 +8,7 @@ const ServicesPageDashboard = () => {
               <div className="card-body">
                 Lorem ipsum dolor sit amet consectetur adipisicing elit.
                 Voluptatem aperiam sed vitae minima officiis similique in
-                obcaecati corrupti, natus facilis.aaa hahaha
+                obcaecati corrupti, natus facilis.aaa hahahassssss
               </div>
             </div>
           </div>
